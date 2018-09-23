ada8cb5e15698fb803c06a36af965a3cd50c34e7
Vivek Kartha <vivek.kartha@gmail.com>
2018-09-24 01:42:06 +0530

    Added scrabble score

---
+++ scrabble-score/scrabble.go
@@ -0,0 +1,24 @@
+package scrabble
+
+import (
+	"strings"
+)
+
+//Score finds the scrable score
+func Score(s string) int {
+	s = strings.ToUpper(s)
+	scores := map[rune]int{
+		'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+		'D': 2, 'G': 2,
+		'B': 3, 'C': 3, 'M': 3, 'P': 3,
+		'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+		'K': 5,
+		'J': 8, 'X': 8,
+		'Q': 10, 'Z': 10,
+	}
+	var count int
+	for _, v := range s {
+		count += scores[v]
+	}
+	return count
+}
